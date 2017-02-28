c8621de6f5f34193c4506d3f58f406d89ee220cb
Ben Kahle <ben@kahle.org>
2017-02-28 12:03:40 -0800

    add base config boilerplate with webpack, babel, sass

---
+++ config/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSassPlugin = new ExtractTextPlugin({
+    filename: "style.css", //Output css for production
+    disable: process.env.NODE_ENV !== "production"
+});
+const copyWebpackPlugin = new CopyWebpackPlugin([
+  {
+    context: "./src/static",
+    from: "**/*"
+  }
+]);
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../build')
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ],
+    rules: [
+      {
+        test: /.scss$/,
+        loader: extractSassPlugin.extract({
+          use: [{
+            loader: "css-loader" //Allow loading css via require
+          }, {
+            loader: "sass-loader" //Compile scss -> css
+          }],
+          // use style-loader in development
+          fallback: "style-loader" //inline style for development
+        })
+      }
+    ]
+  },
+  plugins: [
+    copyWebpackPlugin,
+    extractSassPlugin
+  ]
+};
---
+++ src/scripts/index.js
@@ -0,0 +1,11 @@
+require("../styles/base.scss");
+
+function component () {
+  var element = document.createElement('div');
+
+  element.innerHTML = "Hello, World!";
+
+  return element;
+}
+
+document.body.appendChild(component());
