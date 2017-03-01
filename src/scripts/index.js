051c2b4ab6176c88087569b2cb24ed4c9faf526b
Ben Kahle <ben@kahle.org>
2017-03-01 15:15:40 -0800

    add static image

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,9 @@ function component () {
   var element = document.createElement('div');
 
   element.innerHTML = "Hello, World!";
-
+  var im = document.createElement('img');
+  im.src = "images/earth.jpg";
+  element.appendChild(im);
   return element;
 }
 
